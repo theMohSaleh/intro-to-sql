cf634bf023faefbc597de8a8cacc1c07b05b2404
Mohamed Saleh <mohxbox36021@gmail.com>
2024-12-03 02:34:43 +0300

    finished all clues

--- clues.sql
+++ clues.sql
@@ -16,11 +16,12 @@ SELECT * FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
-
+SELECT c.name FROM cities c, countries d WHERE c.countrycode = 'SMR' AND d.code = 'SMR' AND c.name != d.name;
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+SELECT name FROM cities WHERE countrycode != 'SMR' AND name LIKE 'Marin%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -28,6 +29,7 @@ SELECT * FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
 -- follow right behind you!
 
 -- Write SQL query here
+SELECT name FROM cities WHERE id = (SELECT capital FROM countries WHERE code = 'BRA');
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -40,5 +42,6 @@ SELECT * FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
 --               So I'm off to add one to the population I find
 --               In a city of ninety-one thousand and now, eighty five.
 
+SELECT * FROM cities WHERE population = 91084;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
